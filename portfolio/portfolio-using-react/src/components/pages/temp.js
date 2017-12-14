0f32084482d0cd504ee425f44d10a539beb7e60b
Ahmed Raza <raza403@gmail.com>
2017-12-14 20:36:32 +0500

    Added my code at devbros-tk

---
+++ portfolio/portfolio-using-react/src/components/pages/temp.js
@@ -0,0 +1,9 @@
+<Link to="/" class="navbar-brand">Home</Link>
+            <Link to="/about" className='mr-3 pt-1'>About</Link>
+            <Link to="/portfolio" className='mr-3 pt-1'>Portfolio</Link>
+
+
+
+            <Jumbotron />
+    <AboutPage />
+    <PortfolioPage />
